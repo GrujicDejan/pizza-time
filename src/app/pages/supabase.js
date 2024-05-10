b4bc98dc5d2f588a9ccd5f2bc9d7ece23b8021e8
@Dejan.Grujic <dejan.grujic@synechron.com>
2024-05-10 20:46:41 +0200

    update

--- src/app/pages/supabase.js
+++ src/app/pages/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zacvuhydqunmvxwrgcja.supabase.co";
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphY3Z1aHlkcXVubXZ4d3JnY2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMTYzMTgsImV4cCI6MjAzMDY5MjMxOH0.LVf8g_nKU0qiIe0IdfRi9SGzWHcQs2fL9OwlLqvnztI';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
